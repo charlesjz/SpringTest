c72f31584d2e2b8cb72f7e7a2569856f27546c0d
charlesjz <charles.j.zhu@gmail.com>
2018-02-28 23:46:41 -0500

    2018-02-28

---
+++ src/com/mycompany/TestBean.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestBean {
+
+	public static void main(String[] args) {
+		String conf = "ApplicationContext.xml";
+		ApplicationContext ac = new ClassPathXmlApplicationContext(conf);
+		
+//		Calendar c1= new GregorianCalendar();
+		Calendar c1 = ac.getBean("c1",Calendar.class);
+		System.out.println(c1);
+	}
+
+}
